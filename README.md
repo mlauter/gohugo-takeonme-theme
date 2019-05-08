e65dce186282288ccbe1504b3fea754d385846b9
Miriam Lauter <lauter.miriam@gmail.com>
2019-05-08 09:05:29 -0400

    Working first pass with hero image homepage and 404 page

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 under construction
+
+heavily borrowing from https://github.com/budparr/gohugo-theme-ananke
+
+## Customization
+
+``` toml
+[params]
+  body_classes = "avenir"
+  custom_css = ["css/custom.css","css/custom2.css"]
+```
---
+++ src/webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var AssetsPlugin = require('assets-webpack-plugin');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+	entry: {
+            app: './js/main.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env']
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader?importLoaders=1!postcss-loader'
+				})
+			}
+		]
+	},
+
+	output: {
+		path: path.join(__dirname, './../static/dist'),
+		filename: 'js/[name].[chunkhash].js'
+	},
+
+	resolve: {
+		modules: [path.resolve(__dirname, 'src'), 'node_modules']
+	},
+
+        plugins: [
+                new CleanWebpackPlugin(),
+		new AssetsPlugin({
+			filename: 'webpack_assets.json',
+			path: path.join(__dirname, '../data'),
+			prettyPrint: true
+		}),
+		new ExtractTextPlugin({
+			filename: getPath => {
+				return getPath('css/[name].[contenthash].css');
+			},
+			allChunks: true
+		})
+	],
+	watchOptions: {
+		watch: true
+	}
+};
